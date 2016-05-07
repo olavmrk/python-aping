799f7c86fe90bbbe408584cc32553d8d57e66c89
Olav Morken <olavmrk@gmail.com>
2016-05-07 14:51:06 +0200

    Optimize dispatcher.
    When sending TCP pings, we need to create a TCP raw socket. This socket will
    receive all TCP packets destined for the computer, which can cause a lot of
    overhead.
    
    Optimize the dispatcher so that it should be able to discard packets it does
    not care about as efficently as possible.

--- aping/rawsocket.py
+++ aping/rawsocket.py
@@ -3,6 +3,7 @@ import collections
 import ipaddress
 import random
 import socket
+import struct
 import time
 
 from . import packet
@@ -10,81 +11,160 @@ from . import packet
 class _ReceiveDispatcher(object):
 
     def __init__(self):
-        self._listeners = collections.defaultdict(set)
+        self._listeners = collections.defaultdict(lambda: collections.defaultdict(set))
+
+    def _attach_listener(self, ip_target, protocol_target, callback):
+        self._listeners[ip_target][protocol_target].add(callback)
+
+    def _detach_listener(self, ip_target, protocol_target, callback):
+        self._listeners[ip_target][protocol_target].remove(callback)
+        if not self._listeners[ip_target][protocol_target]:
+            del self._listeners[ip_target][protocol_target]
+        if not self._listeners[ip_target]:
+            del self._listeners[ip_target]
+
+    def _has_listener(self, ip_target, protocol_target):
+        if not ip_target in self._listeners:
+            return False
+        if not protocol_target in self._listeners[ip_target]:
+            return False
+        return True
+
+    @staticmethod
+    def _icmp_target(remote_address, identifier, sequence_number):
+        ip_target = (remote_address.packed, 1)
+        icmp_target = struct.pack('>HH', identifier, sequence_number)
+        return ip_target, icmp_target
+
+    @staticmethod
+    def _tcp_target(remote_address, local_port, remote_port, sequence_number):
+        ip_target = (remote_address.packed, 6)
+        expected_ack = (sequence_number + 1) & 0xffffffff
+        tcp_target = struct.pack('>HHI', remote_port, local_port, expected_ack)
+        return ip_target, tcp_target
+
+    @staticmethod
+    def _split_target(target):
+        if target[0] == socket.IPPROTO_ICMP:
+            return _ReceiveDispatcher._icmp_target(target[1], target[2], target[3])
+        elif target[0] == socket.IPPROTO_TCP:
+            return _ReceiveDispatcher._tcp_target(target[1], target[2], target[3], target[4])
+        else:
+            raise NotImplementedError()
 
     def listener_future(self, target):
+        ip_target, protocol_target = _ReceiveDispatcher._split_target(target)
         future = asyncio.Future()
-        # Attach the queue to the packet listeners
-        self._listeners[target].add(future)
+        self._attach_listener(ip_target, protocol_target, future.set_result)
         def _cleanup(future):
-            self._listeners[target].remove(future)
-            if not self._listeners[target]:
-                # Nothing else listening for this target -- clean it up
-                del self._listeners[target]
+            self._detach_listener(ip_target, protocol_target, future.set_result)
         future.add_done_callback(_cleanup)
         return future
 
-    def _lookup(self, target):
-        # We manually check for the presence of `target` here in order to avoid
-        # a memory leak. If we don't check, the defaultdict backing `self._listeners`
-        # will add a new set-object fot every target we try to look up.
-        if not target in self._listeners:
-            return set()
-        return self._listeners[target]
-
-    def _lookup_icmp(self, source_address, identifier, sequence_number):
-        target = (socket.IPPROTO_ICMP, source_address, identifier, sequence_number)
-        return self._lookup(target)
-
-    def _lookup_tcp(self, destination_address, source_port, destination_port, sequence_number):
-        target = (socket.IPPROTO_TCP, destination_address, source_port, destination_port, sequence_number)
-        return self._lookup(target)
-
-    def _find_inner_payload_futures(self, outer_payload):
+    def _dispatch_target(self, ip_target, protocol_target, raw_packet, timestamp):
+        ip_listeners = self._listeners.get(ip_target, None)
+        if ip_listeners is None:
+            return
+        protocol_listeners = ip_listeners.get(protocol_target, None)
+        if protocol_listeners is None:
+            return
         try:
-            protocol_header = outer_payload.extract_payload()
-            address = protocol_header.destination_address
-            payload = protocol_header.extract_payload(allow_partial=True)
-        except:
-            # Not enough data in the packet to identify it.
-            return set()
-        # Now we have the payload we just sent in `payload` and its destination in
-        # address. Find out what kind of packet this was and try to match it to our
-        # listeners.
-        if isinstance(payload, packet.IcmpEchoRequest):
-            return self._lookup_icmp(address, payload.identifier, payload.sequence_number)
-        elif isinstance(payload, packet.Tcp):
-            return self._lookup_tcp(address, payload.source_port, payload.destination_port, payload.sequence_number)
-        else:
-            return set() # Unknown payload type
-
-    def _find_payload_futures(self, source_address, payload):
-        if isinstance(payload, packet.IcmpEchoReply):
-            return self._lookup_icmp(source_address, payload.identifier, payload.sequence_number)
-        elif isinstance(payload, packet.IcmpTimeExceeded):
-            return self._find_inner_payload_futures(payload)
-        elif isinstance(payload, packet.IcmpDestinationUnreachable):
-            return self._find_inner_payload_futures(payload)
-        elif isinstance(payload, packet.Tcp):
-            sequence_number = (payload.acknowledgment_number - 1) & 0xffffffff
-            return self._lookup_tcp(source_address, payload.destination_port, payload.source_port, sequence_number)
-        else:
-            return set() # Unknown payload type
-
-    def dispatch(self, response, timestamp):
-        try:
-            response = packet.IPv4.from_bytes(response)
+            response = packet.IPv4.from_bytes(raw_packet)
             payload = response.extract_payload()
         except:
             return # Invalid response
-        futures = self._find_payload_futures(response.source_address, payload)
-        for future in futures:
-            future.set_result((timestamp, response))
+        for target in protocol_listeners:
+            target((timestamp, response))
+
+    def _dispatch_icmp_echo(self, raw_packet, timestamp, ip_target, icmp_start):
+        icmp_match = raw_packet[icmp_start+4:icmp_start+8]
+        return self._dispatch_target(ip_target, icmp_match, raw_packet, timestamp)
+
+    def _dispatch_tcp(self, raw_packet, timestamp, ip_target, tcp_start):
+        if len(raw_packet) < tcp_start + 12:
+            return # Too short to contain enough data from the TCP header to route it.
+        port_match = raw_packet[tcp_start:tcp_start+4] # b'<remote_port><local_port>'
+        ack_match = raw_packet[tcp_start+8:tcp_start+12]
+        tcp_match = port_match + ack_match
+        return self._dispatch_target(ip_target, tcp_match, raw_packet, timestamp)
+
+    def _dispatch_tcp_error(self, raw_packet, timestamp, ip_target, tcp_start):
+        if len(raw_packet) < tcp_start + 8:
+            return # Too short to contain 8 bytes of the original TCP header
+        # For fast matching of incoming TCP datagrams, the match block
+        # for TCP is b'<remote_port><local_port><expected_ack>'
+        # Now we need to transform our outgoing datagram into this form.
+        local_port_bytes = raw_packet[tcp_start:tcp_start+2]
+        remote_port_bytes = raw_packet[tcp_start+2:tcp_start+4]
+        sequence_number_bytes = raw_packet[tcp_start+4:tcp_start+8]
+        sequence_number = struct.unpack('>I', sequence_number_bytes)[0]
+        expected_ack = (sequence_number + 1) & 0xffffffff
+        expected_ack_bytes = struct.pack('>I', expected_ack)
+        tcp_match = remote_port_bytes + local_port_bytes + expected_ack_bytes
+        return self._dispatch_target(ip_target, tcp_match, raw_packet, timestamp)
+
+    def _dispatch_ipv4_icmp_error_icmp(self, raw_packet, timestamp, ip_target, icmp_start):
+        if len(raw_packet) < icmp_start + 8:
+            return # Too short to contain 8 bytes of the original ICMP header
+        icmp_type = raw_packet[icmp_start]
+        if icmp_type != 8:
+            return # Not a transmitted echo request
+        return self._dispatch_icmp_echo(raw_packet, timestamp, ip_target, icmp_start)
+
+    def _dispatch_ipv4_icmp_error(self, raw_packet, timestamp, icmp_start):
+        ip_start = icmp_start + 8
+        if len(raw_packet) < ip_start + 20:
+            return # Too short to contain the inner IP header
+        remote_address = raw_packet[ip_start+16:ip_start+20]
+        protocol = raw_packet[ip_start + 9]
+        ip_target = (remote_address, protocol)
+        protocol_start = ip_start + (raw_packet[ip_start] & 0b00001111) * 4
+        if protocol == 1:
+            return self._dispatch_ipv4_icmp_error_icmp(raw_packet, timestamp, ip_target, protocol_start)
+        elif protocol == 6:
+            return self._dispatch_tcp_error(raw_packet, timestamp, ip_target, protocol_start)
+
+    def _dispatch_ipv4_icmp(self, raw_packet, timestamp, ip_target, icmp_start):
+        if len(raw_packet) < icmp_start + 8:
+            return # Too short to contain the 8-byte ICMP header
+        icmp_type = raw_packet[icmp_start]
+        if icmp_type == 0:
+            return self._dispatch_icmp_echo(raw_packet, timestamp, ip_target, icmp_start)
+        elif icmp_type == 3: # Destination unreachable
+            return self._dispatch_ipv4_icmp_error(raw_packet, timestamp, icmp_start)
+        elif icmp_type == 11: # Time exceeded
+            return self._dispatch_ipv4_icmp_error(raw_packet, timestamp, icmp_start)
+        else:
+            return # Unknown ICMP type
+
+    def _dispatch_ipv4(self, raw_packet, timestamp):
+        if len(raw_packet) < 20:
+            return # Too short to contain the full IPv4 header
+        protocol = raw_packet[9]
+        protocol_start = (raw_packet[0] & 0b00001111) * 4
+        remote_address = raw_packet[12:16]
+        ip_target = (remote_address, protocol)
+        if protocol == 1:
+            return self._dispatch_ipv4_icmp(raw_packet, timestamp, ip_target, protocol_start)
+        elif protocol == 6:
+            return self._dispatch_tcp(raw_packet, timestamp, ip_target, protocol_start)
+        else:
+            return # Protocol not implemented
+
+    def dispatch(self, raw_packet, timestamp):
+        if len(raw_packet) < 1:
+            return # Not enough data to identify packet
+        ip_protocol = (raw_packet[0] >> 4)
+        if ip_protocol == 4:
+            return self._dispatch_ipv4(raw_packet, timestamp)
+        else:
+            return # Protocol not implemented
 
     def find_free_icmp_sequence_number(self, target, identifier):
         while True:
             sequence_number = random.randrange(0x10000)
-            if self._lookup_icmp(target, identifier, sequence_number):
+            ip_target, protocol_target = _ReceiveDispatcher._icmp_target(target, identifier, sequence_number)
+            if self._has_listener(ip_target, protocol_target):
                 # Something else is already using this sequence number
                 continue
             return sequence_number
@@ -92,7 +172,8 @@ class _ReceiveDispatcher(object):
     def find_free_tcp_sequence_number(self, target, source_port, destination_port):
         while True:
             sequence_number = random.randrange(0x100000000)
-            if self._lookup_tcp(target, source_port, destination_port, sequence_number):
+            ip_target, protocol_target = _ReceiveDispatcher._tcp_target(target, source_port, destination_port, sequence_number)
+            if self._has_listener(ip_target, protocol_target):
                 # Something else is already using this sequence number
                 continue
             return sequence_number
